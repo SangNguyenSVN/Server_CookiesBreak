296e8ba12a3ab10acc0948d248c382af00655c4e
SangNguyenSVN <nguyenvusang1108@gmail.com>
2024-09-17 15:46:33 +0700

    'sang'

--- server.js
+++ server.js
@@ -15,23 +15,23 @@ mongoose
   .then(() => console.log('MongoDB connected successfully'))
   .catch((err) => console.error('MongoDB connection error:', err));
 
-// Set up Handlebars as the view engine
-app.set('view engine', 'hbs');
 
-// Set the views directory
+app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
-// Error-handling middleware (optional, if you want to handle errors globally)
+
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Something went wrong!');
 });
 
-// Import routes
+
 const indexRouter = require('./routes/index');
+const productRoute= require('./routes/product')
 
-// Use the routes from routes/index.js
+// http://localhost:3000
 app.use('/', indexRouter);
+app.use('/product', productRoute)
 
-// Export the app for the server file
+
 module.exports = app;
