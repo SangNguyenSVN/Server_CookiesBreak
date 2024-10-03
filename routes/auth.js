123669e6fed9080fec4a1a20721c3c435d46d134
SangNguyenSVN <nguyenvusang1108@gmail.com>
2024-10-03 19:57:15 +0700

    'sang'

--- routes/auth.js
+++ routes/auth.js
@@ -2,8 +2,11 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controller/auth');
 
+// http://localhost:3000/auth/register/patient
 router.post('/register/patient', authController.registerPatient);
+// http://localhost:3000/auth/register/doctor
 router.post('/register/doctor', authController.registerDoctor);
+// http://localhost:3000/auth/login
 router.post('/login', authController.login);
 
 module.exports = router;
